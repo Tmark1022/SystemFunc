b2e9aeb1c7c4a0f96bb02a9c33aefc70a930d95f
tmark's company <779297395@qq.com>
2020-05-11 19:04:11 +0800

    内存对齐和结构体成员offset等测试代码

--- internet/struct_align/test2.c
+++ internet/struct_align/test2.c
@@ -1,14 +1,10 @@
 /*************************************************************************
  @ Author	: tmark
- @ Created Time	: Mon 11 May 2020 04:01:15 PM CST
- @ File Name	: test.c
+ @ Created Time	: Mon 11 May 2020 05:32:03 PM CST
+ @ File Name	: test2.c
  @ Description	: 
  ************************************************************************/
-// set packing size, suppress padding
-// pragma pack(n), individual member 的size小于n还是按照自身的size大小进行内存对齐，大于n的就按照n进行。
-// Expected #pragma pack parameter to be '1', '2', '4', '8', or '16' on gcc compiler
 #pragma pack(4)
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,41 +12,46 @@
 #include <unistd.h>
 #include <stddef.h>
 
-// 16
-struct A {
-	char c;		
-	int i;	
-	long l;
-};
-
-struct B {
-	int i;
-	char c;		
-	long l;
-};
-
+// 24
 struct C {
 	int i;
 	long l;
 	char c;		
 };
 
-struct D {
-	long l;
-	int i;
-	char c;		
+// 最大类型long, 整个结构体size是8的整数倍
+struct A{
+	char a;			// 0
+	short b;		// 2
+	int c;			// 4
+	long d;			// 8
+	int e;			// 16
+	short f;		// 20
+	char g;			// 21
+};
+
+// 64
+struct B{
+	char a;			// 0
+	short b;		// 2
+	char c[10];		// 3
+	long d;			// 16
+	struct C e;		// 24
+	double f;		// 48
+	short g;		// 56
 };
 
 #define PRINT_STRUCT(x) do { printf(#x", size = %ld, c offset is %ld, i offset is %ld, l offset is %ld\n", \
 		sizeof(x), offsetof(x, c), offsetof(x, i), offsetof(x, l));} while(0)
 
+#define PRINT_STRUCT2(x) do { printf(#x", size = %ld, a offset is %ld, b offset is %ld, c offset is %ld, d offset is %ld, e offset is %ld, f offset is %ld, g offset is %ld\n", sizeof(x), offsetof(x, a), offsetof(x, b), offsetof(x, c), offsetof(x, d), offsetof(x, e), offsetof(x, f), offsetof(x, g));} while(0)
 
 int main(int argc, char *argv[]) {
 	printf("short = %ld, int = %ld, long = %ld, long long = %ld, double = %ld, float = %ld\n", sizeof(short), sizeof(int), sizeof(long), sizeof(long long), sizeof(double), sizeof(float));
 	
-	PRINT_STRUCT(struct A);
-	PRINT_STRUCT(struct B);
 	PRINT_STRUCT(struct C);
-	PRINT_STRUCT(struct D);
+	PRINT_STRUCT2(struct A);
+	PRINT_STRUCT2(struct B);
 	return 0;
 }
+
