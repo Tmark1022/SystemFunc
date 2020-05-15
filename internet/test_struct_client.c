ecbcd7403edae20c6faca6fbe7aff299d1a26597
tmark's company <779297395@qq.com>
2020-05-15 15:31:32 +0800

    网络编程部分函数

--- internet/test_struct_client.c
+++ internet/test_struct_client.c
@@ -7,7 +7,82 @@
 #include "wrap/wrap.h"
 #include "test_struct.h"
 
+char inbuf[1024];
+char outbuf[10240];
+
+int DoInput(Proto * pto) 
+{
+	if (NULL == pto) {
+		exit(EXIT_FAILURE);	
+	}
+
+	int type;
+	while (1) {
+		scanf("%d", &type);
+		getchar();
+		if (type != TYPE_CALL && type != TYPE_ECHO) {
+			printf("type error, repeat input\n");
+			continue;
+		}
+
+		if (NULL == fgets(inbuf, sizeof(inbuf), stdin)) {
+			exit(EXIT_FAILURE);
+		}
+		
+		if (strlen(inbuf) > 0) {
+			if (inbuf[strlen(inbuf) - 1] == '\n') {
+				// 去掉换行
+				inbuf[strlen(inbuf) - 1] = '\0';
+			}
+		}	
+
+		pto->type = type;	
+		pto->size = strlen(inbuf);
+
+		break;
+	}		
+
+	return 1;
+}
+
 int main(int argc, char *argv[]) {
 
+	int sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+	if (-1 == sockfd) {
+		perror("call socket failed");
+		exit(EXIT_FAILURE);
+	}
+
+	struct sockaddr_in serverSockAddr;
+	serverSockAddr.sin_family = AF_INET;
+	serverSockAddr.sin_port = htons(8888);
+	inet_pton(AF_INET, "127.0.0.1", &serverSockAddr.sin_addr);
+	Connect(sockfd, (const struct sockaddr *)&serverSockAddr, sizeof(struct sockaddr_in));
+
+
+	Proto pto;
+	int ret;
+	while(1) {	
+		DoInput(&pto); 
+
+		
+		// 先写头，然后写数据部分	
+		WriteCount(sockfd, &pto, sizeof(Proto));
+		WriteCount(sockfd, inbuf, strlen(inbuf));
+	
+		ret = read(sockfd, outbuf, sizeof(outbuf) - 1);
+		if (-1 == ret) {
+			perror("read error");
+			exit(EXIT_FAILURE);
+		}	
+
+		// 尾部出入\0
+		outbuf[ret] = '\0';	
+		printf("%s\n", outbuf);
+			
+	}
+	
+	close(sockfd);
+	
 	return 0;
 }
--- internet/test_struct_server.c
+++ internet/test_struct_server.c
@@ -9,16 +9,80 @@
 #include "wrap/wrap.h"
 #include "test_struct.h"
 
+int stdoutFd;
+
 void DoReadWrite(int cfd) 
 {
 	int cnt;
 	Proto pto;
-	/*
-	while ((cnt = ReadCount(cfd, buf, 1024)) > 0) {
-		printf("read cnt is %d\n, get string :%s\n", cnt, buf);	
-		WriteCount(cfd, buf, cnt);
+
+	while ((cnt = ReadCount(cfd, &pto, sizeof(Proto))) > 0) {
+		if (cnt != sizeof(Proto)) {
+			fprintf(stderr, "cnt != sizeof(Proto)\n");
+			exit(EXIT_FAILURE);
+		}
+		
+		// invalid type	
+		if (pto.type != TYPE_ECHO && pto.type != TYPE_CALL) {
+			fprintf(stderr, "type(%d) error\n", pto.type);
+			exit(EXIT_FAILURE);
+		}	
+
+		// 后续的内容读出来
+		int size = pto.size;	
+		if (size < 0) {
+			fprintf(stderr, "pto size error\n");
+			exit(EXIT_FAILURE);
+		}
+	
+		printf("get pto (%d, %d)\n", pto.type, pto.size);
+
+		if (0 == size) {
+			continue;
+		}	
+		
+		// +1 兼容最后一个\0
+		char * tmp = malloc(size + 1); 	
+		if (NULL == tmp) {
+			fprintf(stderr, "malloc error \n");
+			exit(EXIT_FAILURE);
+
+		}
+
+		cnt = ReadCount(cfd, tmp, size);
+		if (cnt != size) {
+			fprintf(stderr, "miss content\n");
+			continue;
+		}
+		
+		if (pto.type == TYPE_ECHO) {	
+			WriteCount(cfd, tmp, cnt);
+
+			tmp[cnt] = '\0'; 
+			printf("get string : %s\n", tmp);
+
+		} else if (pto.type == TYPE_CALL) {
+			// 重定向标准输出到连接到的client	
+			if ( -1 == dup2(cfd, STDOUT_FILENO)) {
+				perror("dup2 error");
+				exit(EXIT_FAILURE);
+			}
+			
+			tmp[cnt] = '\0';
+			int status = system(tmp);
+
+			// 恢复标准输出
+			if ( -1 == dup2(stdoutFd, STDOUT_FILENO)) {
+				perror("dup2 error, 2222222");
+				exit(EXIT_FAILURE);
+			}
+			
+			printf("call : %s\nreturn status : %d\n", tmp, status);
+		}
+
+		free(tmp);
+		tmp = NULL;
 	}
-	*/
 }
 
 int main(int argc, char * argv[]) 
@@ -42,6 +106,13 @@ int main(int argc, char * argv[])
 	struct sockaddr_in clientAddr;
 	int cfd = Accept(fd, (struct  sockaddr * )&clientAddr, &addrlen);
 	
+	// dup标准输出，保存一份
+	stdoutFd = dup(STDOUT_FILENO);
+	if (-1 == stdoutFd) {
+		perror("dup error");
+		exit(EXIT_FAILURE);
+	}
+
 	DoReadWrite(cfd);
 
 	close(cfd);	
