e4f5f98264253703c1950e2e2406fe3616d56965
tmark's company <779297395@qq.com>
2020-06-03 17:23:39 +0800

    libevent 测试代码

--- libevent/test3.c
+++ libevent/test3.c
@@ -28,17 +28,26 @@ void PrintError(FILE * stream, int my_errno, const char * headStr, int exitCode)
 	}
 }
 
-void timeout1(evutil_socket_t fd, short what, void * arg)
+
+void active_cb(evutil_socket_t fd, short what, void * arg)
 {
 	char * aa = (char *) arg;
-	printf("i am t1, fd %d, what %x, arg : %s\n", fd, what, aa);
+	printf("i am active_cb, fd %d, what %x, arg : %s\n", fd, what, aa);
+}
+
+void timeout1(evutil_socket_t fd, short what, void * arg)
+{
+	struct event  * aa = (struct event *) arg;
+	printf("i am t1, fd %d, what %d\n", fd, what);	
+
+	event_active(aa, EV_READ | EV_WRITE | EV_TIMEOUT, 0);
 }
 
 void timeout2(evutil_socket_t fd, short what, void * arg)
 {
 	struct event * event = (struct event *)arg; 
 	event_del(event);
-	printf("i am t2, fd %d, what %x, try to exit\n", fd, what);
+	printf("i am t2, fd %d, what %d, try to exit\n", fd, what);
 }
 
 int main(int argc, char *argv[]) {
@@ -48,7 +57,10 @@ int main(int argc, char *argv[]) {
 		PrintError(stderr, -1, "event_base_new error", EXIT_FAILURE);				
 	}
 	
-	struct event *event = event_new(base, -1, EV_TIMEOUT | EV_PERSIST, timeout1, "i am timeout 1");  		
+
+
+	struct event *aa = event_new(base, -1, EV_TIMEOUT | EV_PERSIST, active_cb, "hahaha");  		
+	struct event *event = event_new(base, -1, EV_TIMEOUT | EV_PERSIST, timeout1, aa);  		
 	struct timeval t1 = {1,0};
 	event_add(event, &t1);
 	
