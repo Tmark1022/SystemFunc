bb4bbca28d48e3595a14b145761e5a2a6155029e
tmark's company <779297395@qq.com>
2026-03-24 16:45:32 +0800

    wait_tmark

--- process_func/wait_tmark.c
+++ process_func/wait_tmark.c
@@ -11,21 +11,36 @@ int main(int argc, char *argv[]) {
 		perror("fork error");
 	} else if (0 == pid) {
 		// child
-		sleep(100);
+		//sleep(100);
+		while(1);
+		printf("------ lalala\n");
 		return 10;	
 	} else {
 		// parent
 		int wpid, status;	
-		wpid = wait(&status);
-		if (-1 == wpid) {
-			perror("wait error");
-		}
-		if (WIFEXITED(status)) {
-			printf("wpid %d, return code is %d\n", wpid, WEXITSTATUS(status));	
-		}
-		if (WIFSIGNALED(status)) {
-			printf("wpid %d, sig is %d\n", wpid, WTERMSIG(status));	
-		}
+		printf("child pid is %d\n", pid);
+		while(1) {
+			// wpid = wait(&status);
+			wpid = waitpid(-1, &status, WUNTRACED|WCONTINUED);
+			if (-1 == wpid) {
+				perror("wait error");
+			}
+			if (WIFEXITED(status)) {
+				printf("wpid %d, return code is %d\n", wpid, WEXITSTATUS(status));	
+				break;
+			}
+			if (WIFSIGNALED(status)) {
+				printf("wpid %d, sig is %d\n", wpid, WTERMSIG(status));	
+				break;
+			}
+			if (WIFSTOPPED(status)) {
+				printf("wpid %d, stop sig is %d\n", wpid, WSTOPSIG(status));	
+			}
+			if (WIFCONTINUED(status)) {
+				printf("wpid %d, continue", wpid);	
+			}
+		}					
+			
 	}
 
 	return 0;
